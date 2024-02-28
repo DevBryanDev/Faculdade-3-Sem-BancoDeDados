24bca9819ba1b10be16673c0665c8281d365cd8d
Bryan Gabriel <123982269+DevBryanDev@users.noreply.github.com>
2024-02-28 12:39:47 +0000

    EX04

---
+++ List01EX04.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace;
+
+int main() {
+    // Declaração da variável para armazenar o salário
+    double salario;
+
+    // Solicitação do salário ao usuário
+    cout << "Digite o salário do funcionário: R$ ";
+    cin >> salario;
+
+    // Verificação das condições e aplicação do reajuste
+    if (salario < 1000) {
+        salario *= 1.15;  // Reajuste de 15%
+    } else if (salario >= 1000 && salario <= 1500) {
+        salario *= 1.10;  // Reajuste de 10%
+    } else if (salario > 1500) {
+        salario *= 1.05;  // Reajuste de 5%
+    }
+
+    // Exibição do novo salário
+    cout << "Novo salário do funcionário: R$ " << salario << endl;
+
+    return 0;
+}
